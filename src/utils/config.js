49a5fd7b74aeda50ff6bbed24b6b2848c0dbbe54
tiendatttt234 <nguyentiendat23402@gmail.com>
2025-03-04 23:04:20 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 //portport
-export const BASE_URL = "http://localhost:4000/api/v1";
-export const API_URL = "http://localhost:4000/api/v1";
+export const BASE_URL = "https://gogo-be.onrender.com/api/v1";
+export const API_URL = "https://gogo-be.onrender.com/api/v1";
